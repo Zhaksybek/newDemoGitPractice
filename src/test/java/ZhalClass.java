64e56014b1c8d8853f01fb47c3e491d114061a75
Zhaksybek <zhaks18121993@gmail.com>
2022-11-28 20:51:32 -0600

    I added condition

--- src/test/java/ZhalClass.java
+++ src/test/java/ZhalClass.java
@@ -2,11 +2,11 @@ public class ZhalClass {
     public static void main(String[] args) {
         System.out.println("As a branch Zhak I add new linvhkes of code");
         System.out.println("As a branch Zhak I add new lines of code");
-        System.out.println("As a hkbranch Zhak I add new lines of code");
-        System.out.println("As a branch Zhak I add new lines of code");
-        System.out.println("As a brakhknch Zhak I vhkadd new lines of code");
-        System.out.println("As a branch Zhak I add new lines of code");
-        System.out.println("As a brakhnch Zhak I add new lines of code");
-        System.out.println("As a bravhknch Zhak I add new lines of code");
+        int a=0;
+        if(  a==1 || a>= 21){
+            System.out.println("Hello world");
+        }else{
+            System.out.println("Welcome to Chicago");
+        }
     }
 }
